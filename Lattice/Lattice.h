8376f90cf516b837c6da3b8268c3441f714e5242
Fabian Canas <fcanas@gmail.com>
2015-10-10 21:13:15 -0400

    Initial commit

---
+++ Lattice/Lattice.h
@@ -0,0 +1,19 @@
+//
+//  Lattice.h
+//  Lattice
+//
+//  Created by Fabian Canas on 10/10/15.
+//  Copyright © 2015 Fabián Cañas. All rights reserved.
+//
+
+#import <Cocoa/Cocoa.h>
+
+//! Project version number for Lattice.
+FOUNDATION_EXPORT double LatticeVersionNumber;
+
+//! Project version string for Lattice.
+FOUNDATION_EXPORT const unsigned char LatticeVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <Lattice/PublicHeader.h>
+
+
